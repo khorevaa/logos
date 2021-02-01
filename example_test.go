0d0e002c95baf6efd7440aa7fee2910ea7c9f6d5
khorevaa <khorevaa@gmail.com>
2021-02-01 18:47:45 +0300

    fix: add examples

--- example_test.go
+++ example_test.go
@@ -1,16 +1,66 @@
 package logos_test
 
 import (
+	"errors"
 	"github.com/khorevaa/logos"
 )
 
-func ExampleNew() {
+func ExampleNew_simple() {
 
-	log := logos.New("github.com/v8platform/test")
-	log.Info("Error")
-	log.Sync()
+	log := logos.New("<your-package-name>") // like github.com/khorevaa/logos
+	log.Info("This is me first log. Hello world logging systems")
 
-	// Output:
-	// 2021-01-28T22:10:37.059+0300	INFO	github.com/v8platform/test	Error	{"url": "url", "attempt": 3, "backoff": 1}
+}
+
+func ExampleNew_with_config_json() {
+
+	rawConfig := `
+appenders:
+  console:
+    - name: CONSOLE
+      target: stdout
+      encoder:
+        json:
+
+loggerConfigs:
+  root:
+    level: info
+    appender_refs:
+      - CONSOLE
+`
+
+	logos.InitWithConfigContent(rawConfig)
+
+	log := logos.New("<your-package-name>") // like github.com/khorevaa/logos
+	log.Info("This is me first log. Hello world logging systems")
+
+}
+func ExampleNew_with_color_scheme() {
+
+	rawConfig := `
+appenders:
+  console:
+    - name: CONSOLE
+      target: stdout
+      encoder:
+        console:
+          color_scheme:
+            info_level: blue+b
+            debug_level: green+b
+
+loggerConfigs:
+  root:
+    level: debug
+    appender_refs:
+      - CONSOLE
+`
+
+	logos.InitWithConfigContent(rawConfig)
+
+	log := logos.New("<your-package-name>") // like github.com/khorevaa/logos
+	log.Info("This is me first log. Hello world logging systems")
+
+	err := errors.New("log system error")
+	log.Debug("This is me first error", logos.Any("err", err))
 
 }
